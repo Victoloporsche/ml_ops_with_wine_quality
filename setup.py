6e5c3024c3b64a90915c486bf811075a58e60876
Victor Uwaje <v.uwaje@enviosystems.com>
2022-02-22 16:29:30 +0100

    create setup files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version='0.0.1',
+    description='Mlops with WIne Q',
+    author='Victor Uwaje',
+    packages=find_packages(),
+    license='MIT'
+)
+
