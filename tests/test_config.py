50eadfb9497b5e07d9e3f62c3257885d4b603fe0
Victor Uwaje <v.uwaje@enviosystems.com>
2022-02-23 09:08:54 +0100

    update requirements and tox file

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,21 @@
+import pytest
+
+
+class NotInRAnge(Exception):
+    def __init__(self, message='Value not in range'):
+        self.message = message
+        super().__init__(self.message)
+
+
 def test_generic():
+    a = 5
+
+    with pytest.raises(NotInRAnge):
+        if a not in range(10, 20):
+            raise NotInRAnge
+
+
+def test_something():
     a = 2
     b = 2
-    assert a == b
+    assert True
